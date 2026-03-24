2b069f60488bc1b0cf8b4019f281bba89940b0ca
akashci008 <2024ci_akashdeeppanda_a@nie.ac.in>
2026-03-24 15:23:49 +0530

    changes.

--- vaccum_agent1.py
+++ vaccum_agent1.py
@@ -6,11 +6,7 @@ def reflex_vacuum_agent(location, status):
     elif location == "B":
         return "Left"
 
-# Test the reflex agent
-# Environment: location A is dirty, location B is clean
 location = "A"
 status = "Dirty"
-
-# Agent's action
 action = reflex_vacuum_agent(location, status)
 print(f"Location: {location}, Status: {status}, Action: {action}")
