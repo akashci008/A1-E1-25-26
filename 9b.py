ed0c7a89098f1be957be6c93a5b2e2263e1bff92
akashci008 <2024ci_akashdeeppanda_a@nie.ac.in>
2026-04-28 15:04:38 +0530

    Update 9b.py
    updates made in 9b

--- 9b.py
+++ 9b.py
@@ -1,15 +1,12 @@
-
 joint = [
     [0.5, 0.3],
     [0.3, 0.4]
 ]
 
-# Marginal probability of A (row sum)
 print("Marginal Probability of A:")
 for i in range(len(joint)):
     print(f"P(A{i}) =", sum(joint[i]))
-
-# Marginal probability of B (column sum)
+    
 print("\nMarginal Probability of B:")
 for j in range(len(joint[0])):
     col_sum = 0
